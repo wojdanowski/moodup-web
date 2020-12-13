bda290d423330ab2a12f2a2da78f28f355682975
wojdanowski <s.wojdanowski@gmail.com>
2020-12-13 15:55:31 +0100

    home page layout

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,25 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import Loader from './../components/uiElements/Loader';
 import axios from './../utils/axios';
 import { connect } from 'react-redux';
+import foodImg from './../assets/img/food_tc.jpg';
 
 function Home(props) {
 	const [shouldFetch, setShouldFetch] = useState(true);
 	const [recipes, setRecipes] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
 	const { token } = props;
 
 	const onSuccess = (res) => {
 		setRecipes(res.data.data.data);
+		setIsLoading(false);
 		console.log(res);
 	};
 
 	const onFail = (err) => {
+		setIsLoading(false);
 		alert(err);
 	};
 
 	useEffect(() => {
 		if (shouldFetch && token) {
+			setIsLoading(true);
 			axios
 				.get('/api/v1/recipes/', {
 					headers: {
@@ -36,78 +46,50 @@ function Home(props) {
 		}
 	}, [shouldFetch, token]);
 
-	return (
-		<React.Fragment>
-			<Button variant='outline-primary'>some button</Button>
-			<h1>home </h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ad
-				non modi, molestias eveniet perspiciatis sapiente iste libero
-				laborum, excepturi porro amet vitae, totam obcaecati vel.
-				Provident quisquam nihil expedita?
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Adipisci maxime aliquid eos repudiandae ea repellendus tempore
-				accusamus quas consequuntur qui. Aperiam assumenda tempore rerum
-				voluptatum similique a, quam nulla, facere ipsa mollitia
-				distinctio, doloribus culpa! Possimus rem quibusdam quisquam
-				aperiam voluptatibus sequi corrupti praesentium eaque deserunt
-				adipisci? Molestiae, minima mollitia, neque impedit suscipit
-				sint dolorem culpa, cum tempora pariatur velit recusandae?
-				Similique at laborum consectetur omnis ipsum obcaecati aperiam,
-				error blanditiis? Quo enim placeat adipisci possimus dignissimos
-				corporis aliquid non quibusdam nam. Aliquam quas temporibus
-				incidunt ex sed, praesentium accusantium minima saepe porro
-				placeat vero exercitationem quos, magni distinctio dolores.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Adipisci maxime aliquid eos repudiandae ea repellendus tempore
-				accusamus quas consequuntur qui. Aperiam assumenda tempore rerum
-				voluptatum similique a, quam nulla, facere ipsa mollitia
-				distinctio, doloribus culpa! Possimus rem quibusdam quisquam
-				aperiam voluptatibus sequi corrupti praesentium eaque deserunt
-				adipisci? Molestiae, minima mollitia, neque impedit suscipit
-				sint dolorem culpa, cum tempora pariatur velit recusandae?
-				Similique at laborum consectetur omnis ipsum obcaecati aperiam,
-				error blanditiis? Quo enim placeat adipisci possimus dignissimos
-				corporis aliquid non quibusdam nam. Aliquam quas temporibus
-				incidunt ex sed, praesentium accusantium minima saepe porro
-				placeat vero exercitationem quos, magni distinctio dolores.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Adipisci maxime aliquid eos repudiandae ea repellendus tempore
-				accusamus quas consequuntur qui. Aperiam assumenda tempore rerum
-				voluptatum similique a, quam nulla, facere ipsa mollitia
-				distinctio, doloribus culpa! Possimus rem quibusdam quisquam
-				aperiam voluptatibus sequi corrupti praesentium eaque deserunt
-				adipisci? Molestiae, minima mollitia, neque impedit suscipit
-				sint dolorem culpa, cum tempora pariatur velit recusandae?
-				Similique at laborum consectetur omnis ipsum obcaecati aperiam,
-				error blanditiis? Quo enim placeat adipisci possimus dignissimos
-				corporis aliquid non quibusdam nam. Aliquam quas temporibus
-				incidunt ex sed, praesentium accusantium minima saepe porro
-				placeat vero exercitationem quos, magni distinctio dolores.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Adipisci maxime aliquid eos repudiandae ea repellendus tempore
-				accusamus quas consequuntur qui. Aperiam assumenda tempore rerum
-				voluptatum similique a, quam nulla, facere ipsa mollitia
-				distinctio, doloribus culpa! Possimus rem quibusdam quisquam
-				aperiam voluptatibus sequi corrupti praesentium eaque deserunt
-				adipisci? Molestiae, minima mollitia, neque impedit suscipit
-				sint dolorem culpa, cum tempora pariatur velit recusandae?
-				Similique at laborum consectetur omnis ipsum obcaecati aperiam,
-				error blanditiis? Quo enim placeat adipisci possimus dignissimos
-				corporis aliquid non quibusdam nam. Aliquam quas temporibus
-				incidunt ex sed, praesentium accusantium minima saepe porro
-				placeat vero exercitationem quos, magni distinctio dolores.
-			</p>
-		</React.Fragment>
-	);
+	let content;
+	if (isLoading && !recipes) {
+		content = (
+			<Container
+				fluid
+				className='d-flex min-vh-100 justify-content-center align-items-center'
+			>
+				<Loader />
+			</Container>
+		);
+	} else if (recipes) {
+		content = (
+			<React.Fragment>
+				<Container fluid>
+					<h2>Latest Recipes</h2>
+					<Row xs={1} sm={1} md={3}>
+						{recipes.map((recipe) => (
+							<Col key={recipe.id}>
+								<Card className='mb-3 border-0'>
+									<Card.Img
+										src={foodImg}
+										className='rounded '
+									/>
+									<Card.ImgOverlay className='with-gradient rounded'></Card.ImgOverlay>
+									<Card.ImgOverlay>
+										<Card.Body className='d-flex h-100 flex-column justify-content-end'>
+											<Card.Text className='text-basicGrey'>
+												{recipe.prepTime}
+											</Card.Text>
+											<Card.Title className='text-basicWhite'>
+												{recipe.name}
+											</Card.Title>
+										</Card.Body>
+									</Card.ImgOverlay>
+								</Card>
+							</Col>
+						))}
+					</Row>
+				</Container>
+			</React.Fragment>
+		);
+	}
+
+	return <div>{content}</div>;
 }
 
 const mapStateToProps = (state) => {
